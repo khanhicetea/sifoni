6e8e4e8a7496416e54acfc3ad2ce47cdbd2a1c17
KhanhIceTea <khanhicetea@gmail.com>
2016-05-24 16:34:17 +0700

    Fix SessionServiceProvider

--- src/Sifoni/Provider/SessionServiceProvider.php
+++ src/Sifoni/Provider/SessionServiceProvider.php
@@ -2,17 +2,22 @@
 
 namespace Sifoni\Provider;
 
+use Silex\Application;
+use Silex\Provider\SessionServiceProvider as SilexSessionServiceProvider;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
-use Silex\Application;
-use Silex\Provider\SessionServiceProvider as SilexSessionServiceProvider;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Sifoni\Adapter\SifoniSessionStorage;
-
 
 class SessionServiceProvider extends SilexSessionServiceProvider
 {
+    private $app;
 
     public function register(Application $app)
     {
@@ -54,6 +59,58 @@ class SessionServiceProvider extends SilexSessionServiceProvider
         $app['session.storage.options'] = array();
         $app['session.default_locale'] = 'en';
         $app['session.storage.save_path'] = null;
+    }
+
+    public function onEarlyKernelRequest(GetResponseEvent $event)
+    {
+        $event->getRequest()->setSession($this->app['session']);
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        // bootstrap the session
+        if (!isset($this->app['session'])) {
+            return;
+        }
+
+        $session = $this->app['session'];
+        $cookies = $event->getRequest()->cookies;
+
+        if ($cookies->has($session->getName())) {
+            $session->setId($cookies->get($session->getName()));
+        } else {
+            $session->migrate(false);
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $session = $event->getRequest()->getSession();
+        if ($session && $session->isStarted()) {
+            $session->save();
+
+            $params = session_get_cookie_params();
+
+            $event->getResponse()->headers->setCookie(new Cookie($session->getName(), $session->getId(), 0 === $params['lifetime'] ? 0 : time() + $params['lifetime'], $params['path'], $params['domain'], $params['secure'], $params['httponly']));
+        }
+    }
+
+    public function boot(Application $app)
+    {
+        $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this, 'onEarlyKernelRequest'), 128);
+
+        if ($app['session.test']) {
+            $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this, 'onKernelRequest'), 192);
+            $app['dispatcher']->addListener(KernelEvents::RESPONSE, array($this, 'onKernelResponse'), -128);
+        }
     }
 
 }
